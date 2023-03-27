2c132d939e892cc4ad7fbe817cfd3a9a5b9c36af
yuheesong <98306847+yuheesong@users.noreply.github.com>
2023-03-27 20:50:32 +0900

    Create 음양 더하기.py

---
+++ Lv1/음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]==True:
+            answer=answer+absolutes[i]
+        else:
+            answer=answer-absolutes[i]
+    return answer
