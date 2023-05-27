f9cfd9713caa6388f9195c453d6b71c16cdc6083
yuheesong <thddbgml345@naver.com>
2023-05-27 17:00:27 +0900

    Update H-Index.py

--- Lv2/H-Index.py
+++ Lv2/H-Index.py
@@ -1,2 +1,9 @@
 def solution(citations):
-  return 
+    citations.sort(reverse=True)
+    n=len(citations)
+    cnt=0
+    for i in citations:
+        if cnt>=i:
+            return cnt
+        cnt+=1
+    return n
